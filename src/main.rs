1164a65a98b368a89e9143d7100c59328a55cdd9
Fabio Bonelli <fb@fabiobonelli.it>
2020-05-25 14:38:13 +0200

    Print cartridge info.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
  clippy::cargo,
 )]
 
+mod cartridge;
 mod cpu;
 mod gpu;
 mod input;
@@ -24,6 +25,8 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::surface::Surface;
 
+use cartridge::Cartridge;
+use memory::IORegisters;
 use rand::prelude::*;
 
 use memory::Rom;
@@ -32,66 +35,6 @@ use memory::IORegisters;
 
 use input::{Input,JoypadButton};
 
-enum Cartridge {
-    Type = 0x147,
-}
-
-fn cartridge_info(path: &str) -> Result<(), io::Error> {
-    let rom = match fs::read(path) {
-        Ok(rom) => rom,
-        Err(e) => return Err(e),
-    };
-
-    println!("Title: {:?}", String::from_utf8(rom[0x134..0x143].to_vec()));
-
-
-    // TODO CGB Flag
-    // TODO Licensee Code
-    // TODO SGB Flag
-
-    println!("{:?}", String::from_utf8(rom[0x134..0x143].to_vec()));
-
-    /* Cartridge type */
-    match rom[0x147] {
-        0x00 => println!("ROM ONLY"),
-        0x01 => println!("MBC1"),
-        0x02 => println!("MBC1+RAM"),
-        0x03 => println!("MBC1+RAM+BATTERY"),
-        0x05 => println!("MBC2"),
-        0x06 => println!("MBC2+BATTERY"),
-        0x08 => println!("ROM+RAM"),
-        0x09 => println!("ROM+RAM+BATTERY"),
-        0x0B => println!("MMM01"),
-        0x0C => println!("MMM01+RAM"),
-        0x0D => println!("MMM01+RAM+BATTERY"),
-        0x0F => println!("MBC3+TIMER+BATTERY"),
-        0x10 => println!("MBC3+TIMER+RAM+BATTERY"),
-        0x11 => println!("MBC3"),
-        0x12 => println!("MBC3+RAM"),
-        0x13 => println!("MBC3+RAM+BATTERY"),
-        0x15 => println!("MBC4"),
-        0x16 => println!("MBC4+RAM"),
-        0x17 => println!("MBC4+RAM+BATTERY"),
-        0x19 => println!("MBC5"),
-        0x1A => println!("MBC5+RAM"),
-        0x1B => println!("MBC5+RAM+BATTERY"),
-        0x1C => println!("MBC5+RUMBLE"),
-        0x1D => println!("MBC5+RUMBLE+RAM"),
-        0x1E => println!("MBC5+RUMBLE+RAM+BATTERY"),
-        0xFC => println!("POCKET CAMERA"),
-        0xFD => println!("BANDAI TAMA5"),
-        0xFE => println!("HuC3"),
-        0xFF => println!("HuC1+RAM+BATTERY"),
-        _ => println!("Unknown"),
-    }
-
-    println!("Rom size: {} KBytes", 32 + rom[0x148] * 2);
-
-    println!("Destination: {}", if rom[0x14a] == 0x00 { "JP" } else { "World" });
-    println!("Old licensee: {:x}", rom[0x14b]);
-
-    Ok(())
-}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let boot_rom_path = env::args().nth(1).expect("Boot ROM required");
@@ -99,6 +42,11 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     let boot_rom = fs::read(&boot_rom_path)?;
     let rom = fs::read(&rom_path)?;
+
+    match Cartridge::new(&rom) {
+        Some(cartridge) => println!("Cartridge info\n{}", &cartridge),
+        None => eprintln!("Can't parse cartridge header"),
+    }
 
     let mut mem = memory::Memory::new();
 
