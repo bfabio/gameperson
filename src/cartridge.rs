0622acf83ae690961e13a896d832d542ad80b1f0
Fabio Bonelli <fb@fabiobonelli.it>
2021-06-02 13:00:29 +0200

    refactor: lint cartridge.rs

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -20,6 +20,7 @@ impl Display for Cartridge<'_> {
 }
 
 impl<'a> Cartridge<'a> {
+    #[allow(clippy::too_many_lines)]
     pub fn new(rom: &'a [u8]) -> Option<Self> {
         // ROM is smaller than a cartridge header.
         if rom.len() < 0x150 {
@@ -32,11 +33,11 @@ impl<'a> Cartridge<'a> {
 
         // 0x14b - Old Licensee Code
         // A value of 0x33 means the New License Code at 0x144-0x145 is used instead.
-        let mut licensee_code: String = format!("{:#02x}", rom[0x14b]);
+        let licensee_code: String = format!("{:#02x}", rom[0x14b]);
         let licensee = match rom[0x14b] {
             0x00 => "<none>",
             0x01 => "Nintendo",
-            0x08 => "Capcom",
+            0x08 | 0x38 => "Capcom",
             0x09 => "hot-b",
             0x0a => "jaleco",
             0x0b => "coconuts",
@@ -58,16 +59,12 @@ impl<'a> Cartridge<'a> {
             0x33 => {
                 let new_licensee = str::from_utf8(&rom[0x144..=0x145]);
 
-                if new_licensee.is_err() {
-                    "<invalid data>"
-                } else {
-                    licensee_code = new_licensee.unwrap().to_string();
-
-                    match new_licensee.unwrap() {
+                if let Ok(licensee_code) = new_licensee {
+                    match licensee_code {
                         "00" => "<none>",
                         "01" => "Nintendo R&D1",
                         "08" => "Capcom",
-                        "13" => "Electronic Arts",
+                        "13" | "69" => "Electronic Arts",
                         "18" => "Hudson Soft",
                         "19" => "B-ai",
                         "20" => "Kss",
@@ -79,8 +76,8 @@ impl<'a> Cartridge<'a> {
                         "30" => "Viacom",
                         "31" => "Nintendo",
                         "32" => "Bandai",
-                        "33" => "Ocean/Acclaim",
-                        "34" => "Konami",
+                        "33" | "93" => "Ocean/Acclaim",
+                        "34" | "54" => "Konami",
                         "35" => "Hector",
                         "37" => "Taito",
                         "38" => "Hudson",
@@ -95,7 +92,6 @@ impl<'a> Cartridge<'a> {
                         "51" => "Acclaim",
                         "52" => "Activision",
                         "53" => "American Sammy",
-                        "54" => "Konami",
                         "55" => "Hitech entertainment",
                         "56" => "LJN",
                         "57" => "Matchbox",
@@ -105,7 +101,6 @@ impl<'a> Cartridge<'a> {
                         "61" => "Virgin",
                         "64" => "LucasArts",
                         "67" => "Ocean",
-                        "69" => "Electronic Arts",
                         "70" => "Infogrames",
                         "71" => "Interplay",
                         "72" => "Broderbund",
@@ -119,7 +114,6 @@ impl<'a> Cartridge<'a> {
                         "87" => "Tsukuda ori*",
                         "91" => "Chunsoft",
                         "92" => "Video system",
-                        "93" => "Ocean/Acclaim",
                         "95" => "Varie",
                         "96" => "Yonezawa/s'pal",
                         "97" => "Kaneko",
@@ -127,55 +121,54 @@ impl<'a> Cartridge<'a> {
                         "A4" => "Konami (Yu-Gi-Oh!)",
                         _ => "<unknown new licensee code>",
                     }
+                } else {
+                    "<invalid data>"
                 }
             }
-            0x34 => "Konami",
+            0x34 | 0xa4 => "Konami",
             0x35 => "Hector",
-            0x38 => "Capcom",
-            0x39 => "Banpresto",
+            0x39 | 0x9d | 0xd9 => "Banpresto",
             0x3c => "*entertainment i",
             0x3e => "Gremlin",
             0x41 => "Ubisoft",
-            0x42 => "Atlus",
-            0x44 => "Malibu",
-            0x46 => "Angel",
+            0x42 | 0xeb => "Atlus",
+            0x44 | 0x4d => "Malibu",
+            0x46 | 0xcf => "Angel",
             0x47 => "Spectrum holoby",
             0x49 => "Irem",
-            0x4a => "Virgin",
-            0x4d => "Malibu",
             0x4f => "U.S. gold",
             0x50 => "Absolute",
-            0x51 => "Acclaim",
+            0x51 | 0xb0 => "Acclaim",
             0x52 => "Activision",
             0x53 => "American Sammy",
             0x54 => "Gametek",
             0x55 => "Park place",
-            0x56 => "LJN",
+            0x56 | 0xff => "LJN",
             0x57 => "Matchbox",
             0x59 => "Milton bradley",
             0x5a => "Mindscape",
             0x5b => "Romstar",
-            0x5c => "Naxat soft",
+            0x5c | 0xd6 => "Naxat soft",
             0x5d => "Tradewest",
             0x60 => "Titus",
-            0x61 => "Virgin",
+            0x4a | 0x61 => "Virgin",
             0x67 => "Ocean",
             0x69 => "Electronic arts",
             0x6e => "Elite systems",
             0x6f => "Electro brain",
             0x70 => "Infogrammes",
             0x71 => "Interplay",
-            0x72 => "Broderbund",
+            0x72 => "Br\u{f8}derbund",
             0x73 => "Sculptered soft",
             0x75 => "The sales curve",
             0x78 => "T*hq",
             0x79 => "Accolade",
             0x7a => "Triffix entertainment",
             0x7c => "Microprose",
-            0x7f => "Kemco",
+            0x7f | 0xc2 => "Kemco",
             0x80 => "Misawa entertainment",
             0x83 => "Lozc",
-            0x86 => "Tokuma shoten intermedia",
+            0x86 | 0xc4 => "Tokuma shoten intermedia",
             0x8b => "Bullet-proof software",
             0x8c => "Vic tokai",
             0x8e => "Ape",
@@ -183,18 +176,16 @@ impl<'a> Cartridge<'a> {
             0x91 => "Chun soft",
             0x92 => "Video system",
             0x93 => "Tsuburava",
-            0x95 => "Varie",
+            0x95 | 0xe3 => "Varie",
             0x96 => "Yonezawa/s'pal",
             0x97 => "Kaneko",
             0x99 => "Arc",
             0x9a => "Nihon bussan",
             0x9b => "Tecmo",
             0x9c => "Imagineer",
-            0x9d => "Banpresto",
             0x9f => "Nova",
             0xa1 => "Hori electric",
-            0xa2 => "Bandai",
-            0xa4 => "Konami",
+            0xa2 | 0xb2 => "Bandai",
             0xa6 => "Kawada",
             0xa7 => "Takara",
             0xa9 => "Technos japan",
@@ -202,9 +193,7 @@ impl<'a> Cartridge<'a> {
             0xac => "Toei animation",
             0xad => "Toho",
             0xaf => "Namco",
-            0xb0 => "Acclaim",
             0xb1 => "Ascii or nexoft",
-            0xb2 => "Bandai",
             0xb4 => "Enix",
             0xb6 => "HAL",
             0xb7 => "SNK",
@@ -213,10 +202,8 @@ impl<'a> Cartridge<'a> {
             0xbb => "Sunsoft",
             0xbd => "Sony imagesoft",
             0xbf => "Sammy",
-            0xc0 => "Taito",
-            0xc2 => "Kemco",
+            0xc0 | 0xd0 => "Taito",
             0xc3 => "Squaresoft",
-            0xc4 => "Tokuma shoten intermedia",
             0xc5 => "Data east",
             0xc6 => "Tonkin house",
             0xc8 => "Koei",
@@ -226,15 +213,11 @@ impl<'a> Cartridge<'a> {
             0xcc => "Use",
             0xcd => "Meldac",
             0xce => "*pony canyon or",
-            0xcf => "Angel",
-            0xd0 => "Taito",
             0xd1 => "Sofel",
             0xd2 => "Quest",
             0xd3 => "Sigma enterprises",
             0xd4 => "Ask kodansha",
-            0xd6 => "Naxat soft",
             0xd7 => "Copya systems",
-            0xd9 => "Banpresto",
             0xda => "Tomy",
             0xdb => "Ljn",
             0xdd => "Ncs",
@@ -243,18 +226,15 @@ impl<'a> Cartridge<'a> {
             0xe0 => "Jaleco",
             0xe1 => "Towachiki",
             0xe2 => "Uutaka",
-            0xe3 => "Varie",
             0xe5 => "Epoch",
             0xe7 => "Athena",
             0xe8 => "Asmik",
             0xe9 => "Natsume",
             0xea => "King records",
-            0xeb => "Atlus",
             0xec => "Epic/Sony records",
             0xee => "Igs",
             0xf0 => "A wave",
             0xf3 => "Extreme entertainment",
-            0xff => "LJN",
             _ => "<unknown old licensee code>",
         };
 
@@ -282,6 +262,7 @@ impl<'a> Cartridge<'a> {
             0x16 => "MBC4+RAM",
             0x17 => "MBC4+RAM+BATTERY",
             0x19 => "MBC5",
+
             0x1A => "MBC5+RAM",
             0x1B => "MBC5+RAM+BATTERY",
             0x1C => "MBC5+RUMBLE",
@@ -302,10 +283,10 @@ impl<'a> Cartridge<'a> {
         Some(Self {
             title,
             size,
+            cartridge_type,
             licensee,
             licensee_code,
             destination_market,
-            cartridge_type,
         })
     }
 }
